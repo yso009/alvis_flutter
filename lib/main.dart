87d29095afdea77fb2d150657842df12334021bc
yso009 <yso009@naver.com>
2022-01-18 14:23:41 +0900

    build alarm_info: AlarmInfo_class

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    title: 'Alvis',
+    home: HomePage(),
+    );
+
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("gdgd"),
+      ),
+    );
+  }
+}
+
+
