ffbbc1ded4963d017e88678076bc7f8b6af18c0f
yso009 <yso009@naver.com>
2022-01-20 23:31:06 +0900

    docs theme_data.dart : color_data

---
+++ lib/constants/theme_data.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+
+  //alvis color
+  static Color backColor = Color(0xFFF6f6f6);
+  static Color back2Color = Color(0xFFE5E5E5);
+  static Color containColor = Color(0xFFFFFFFF);
+  static Color dayTextColor = Color(0xFF3282B8);
+  static Color commentColor = Color(0xFF0F4C75);
+  static Color fontColor = Color(0xFF1B262C);
+  static Color settingbackColor = Color(0xFF3282B8);
+  static Color lineColor = Color(0xFFC6DEEE);
+  static Color lineColorHeader = Color(0xFF5397C6);
+  static Color barColor = Color(0xFF2DABCF);
+  static Color barTus = Color(0xFF51A4DB);
+  static Color barWen = Color(0xFF86C3ED);
+  static Color barThs = Color(0xFF90D7EB);
+  static Color barFri = Color(0xFF5DC1DE);
+  static Color barSat = Color(0xFF2DABCF);
+  static Color barSun = Color(0xFF2BC4BD);
+  static Color arrow = Color(0xFF95BDD9);
+}
